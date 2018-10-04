b9d4c740e76a3620829bbf548a9fcedcf7c3e9ed
Charlles Bandeira <charllesbandeira@gmail.com>
2018-10-04 09:40:13 -0300

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'zga-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'zombicide-green-app';
+}
