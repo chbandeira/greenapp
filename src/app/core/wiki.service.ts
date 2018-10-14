f93b273b9e424c32c37feb61f214589344cf5281
Charlles Bandeira <charllesbandeira@gmail.com>
2018-10-14 00:14:55 -0300

    beta release

--- src/app/core/wiki.service.ts
+++ src/app/core/wiki.service.ts
@@ -7,10 +7,14 @@ import { Observable } from 'rxjs';
 })
 export class WikiService {
 
+  private _uri = 'http://zbp.wikia.com/wiki/';
+  // change for above and excute this when running local: ng serve --proxy-config proxy.conf.json
+  // private _uri = '/wiki/';
+
   constructor(private http: HttpClient) { }
 
   background(survivorName: string): Observable<any> {
-    return this.http.get('http://zbp.wikia.com/wiki/' + survivorName.replace(' ', '_'), { responseType: 'text' });
+    return this.http.get(this._uri + survivorName.replace(' ', '_'), { responseType: 'text' });
   }
 
 }
